5f71059687370f0cf9dae58b80f1972fd58ae3da
zsf0428 <zsf@foxmail.com>
2019-09-04 19:14:00 +0800

    项目管理模块

---
+++ gmall-user-manager/src/main/java/com/atguigu/gmall/gmall/usermanager/GmallUserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.gmall.usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallUserManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallUserManagerApplication.class, args);
+	}
+
+}
